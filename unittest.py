fed2e2d6dc5dad7aeb098061e2186108ab82c098
Savin <mak6et12@gmail.com>
2019-09-05 09:46:08 +0300

    initial commit

---
+++ unittest.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+
+try:
+	link = "http://suninjuly.github.io/registration1.html"
+	browser = webdriver.Chrome()
+	browser.get(link)
+
+	name = browser.find_element_by_tag_name(".first_block .form-group:nth-child(1) input")
+	name.send_keys("Vlad")
+	
+	surname = browser.find_element_by_tag_name(".first_block .form-group:nth-child(2) input")
+	surname.send_keys("Savin")
+	
+	email = browser.find_element_by_css_selector(".first_block .form-group:nth-child(3) input")
+	email.send_keys("savin@gmail.com")
+	
+	phone = browser.find_element_by_css_selector(".second_block .form-group:nth-child(1) input")
+	phone.send_keys("89152223333")
+	
+	address = browser.find_element_by_css_selector(".second_block .form-group:nth-child(2) input")
+	address.send_keys("Moscow")
+	
+	send_button = browser.find_element_by_class_name("btn")
+	send_button.click()
+	
+	time.sleep(1)
+
+	welcome_text_elt = browser.find_element_by_tag_name("h1")
+	welcome_text = welcome_text_elt.text
+	
+	assert "Congratulations! You have successfully registered!" == welcome_text
+	
+finally:
+	time.sleep(5)
+	browser.quit()
